21ab31fae56fd3a4b5cd352b2b8e31e171f23bf9
yongtaoge <geyongtao@seu.edu.cn>
2021-03-10 21:00:40 +0800

    fix: res50 and res101 backbone

--- train_res18_3_feat_1_enc_6_dec.sh
+++ train_res18_3_feat_1_enc_6_dec.sh
@@ -1,2 +1,2 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 PORT=11117 ./tools/dist_train.sh \
-configs/top_down/coord_pose/coco_new/res18_3_feat_1_enc_6_dec_coco_256x192_3x.py 8
+configs/coord_pose/coco_new/res18_3_feat_1_enc_6_dec_coco_256x192_3x.py 8
