77d15b0a45a1b2b9054dd1ad3d22601fd48ff4c4
yongtaoge <geyongtao@seu.edu.cn>
2021-03-11 16:55:47 +0800

    fix: ablation

--- train_res18_4_feat_0_enc_6_dec_no_q2q.sh
+++ train_res18_4_feat_0_enc_6_dec_no_q2q.sh
@@ -1,2 +1,2 @@
 CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 PORT=11117 ./tools/dist_train.sh \
-configs/coord_pose/coco_new/res18_4_feat_1_enc_6_dec_no_q2q_coco_256x192_3x.py 8
+configs/coord_pose/coco_new/res18_4_feat_0_enc_6_dec_no_q2q_coco_256x192_3x.py 8
