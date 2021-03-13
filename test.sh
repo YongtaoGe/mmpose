ecb33cd4909842049d5f30107abbd142af2bdc8e
yongtaoge <geyongtao@seu.edu.cn>
2021-03-13 11:01:50 +0800

    fix: ablation

---
+++ test.sh
@@ -0,0 +1,4 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 PORT=11128 ./tools/dist_test.sh \
+configs/coord_pose/coco_new/res18_3_feat_6_enc_6_dec_coco_256x192_3x.py \
+work_dirs/res18_3_feat_6_enc_6_dec_coco_256x192_3x/epoch_50.pth 8 \
+--eval mAP
