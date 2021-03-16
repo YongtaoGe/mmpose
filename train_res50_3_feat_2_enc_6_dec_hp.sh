43759b06c4571fa717bad7370281078924874691
yongtaoge <geyongtao@seu.edu.cn>
2021-03-16 09:58:56 +0800

    fix: ablation

---
+++ train_res50_3_feat_2_enc_6_dec_hp.sh
@@ -0,0 +1,3 @@
+CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 PORT=11120 ./tools/dist_train.sh \
+configs/coord_pose/coco_new/res50_3_feat_2_enc_6_dec_hp_coco_256x192_4x.py 8 \
+#--resume-from work_dirs/res50_3_feat_6_enc_6_dec_hp_coco_256x192_4x/epoch_170.pth
