debc69ebec5ba9d509ff44bdf4b0799de47866cb
证一 <yongtao.gyt@antgroup.com>
2023-09-06 17:36:33 +0800

    fix

--- configs/controlpose/rtmpose-l_8xb256-420e_coco-256x192.py
+++ configs/controlpose/rtmpose-l_8xb256-420e_coco-256x192.py
@@ -78,8 +78,7 @@ model = dict(
         init_cfg=dict(
             type='Pretrained',
             prefix='backbone.',
-            checkpoint='https://download.openmmlab.com/mmpose/v1/projects/'
-            'rtmposev1/cspnext-l_udp-aic-coco_210e-256x192-273b7631_20230130.pth'  # noqa
+            checkpoint='data/pretrain/cspnext-l_udp-aic-coco_210e-256x192-273b7631_20230130.pth'  # noqa
         )),
     head=dict(
         type='RTMCCHead',
--- scripts/train_rtmpose_coco.sh
+++ scripts/train_rtmpose_coco.sh
@@ -1,6 +1,6 @@
 # export NCCL_P2P_DISABLE=1
 NCCL_P2P_DISABLE=1 CUDA_VISIBLE_DEVICES='1,2' ./tools/dist_train.sh \
 configs/controlpose/rtmpose-l_8xb256-420e_coco-256x192.py \
-2 \
+1 \
 
 # ${CONFIG_FILE} ${GPU_NUM} [PY_ARGS]
